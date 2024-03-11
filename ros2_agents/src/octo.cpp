b65c1c15b024bdc51e67e7b8d8055b6ae51f8404
Sebastian Peralta <peraltas@seas.upenn.edu>
2024-03-11 00:00:40 +0000

    dev-latest

--- ros2_agents/src/octo.cpp
+++ ros2_agents/src/octo.cpp
@@ -23,17 +23,18 @@ Octo::Octo(rclcpp::NodeOptions& options) : OctoAgent(options), py_guard_()
   this->declare_parameter("visualize", true);
 
   // Whether or not to load the inference server or just pass through.
-  this->declare_parameter("dummy", false);
-  this->declare_parameter("weights_key", "octo-base");
-  this->declare_parameter("model_type", "octo");
+  // this->declare_parameter("dummy", false);
+  // this->declare_parameter("weights_key", "octo-base");
+  // this->declare_parameter("model_type", "octo");
   this->declare_parameter("py_log_level", "INFO");
+  this->declare_parameter("model_uri", "hf://rail-berkley/octo-base");
+  this->declare_parameter("name", "octo");
 
   // Initialize parameters
-  bool dummy = this->get_parameter("dummy").as_bool();
 
   auto world_frame = this->get_parameter("world_frame").as_string();
-  auto weights_key = this->get_parameter("weights_key").as_string();
-  auto model_type  = this->get_parameter("model_type").as_string();
+  // auto weights_key = this->get_parameter("weights_key").as_string();
+  auto model_uri  = this->get_parameter("model_uri").as_string();
 
   // Initialize rviz_visual_tools
 
@@ -45,8 +46,7 @@ Octo::Octo(rclcpp::NodeOptions& options) : OctoAgent(options), py_guard_()
   // TODO(speralta): Do not do this with the pybind11 embedded interpreter.
   inference_ = py::module::import(
     "robo_transformers.inference_server").attr(
-    "InferenceServer")(py::arg("model_type") = model_type, py::arg("weights_key") = weights_key,
-                       py::arg("dummy")      = dummy);
+    "InferenceServer")(py::arg("model_uri") = model_uri);
   py::module::import("absl").attr("logging").attr("set_verbosity")(this->get_parameter(
                                                                      "py_log_level").as_string());
 
@@ -65,25 +65,46 @@ Mbodied::Feedback::SharedPtr Octo::actionFromObs(std::shared_ptr<OctoObserver>ob
   auto feedback  = std::make_shared<Mbodied::Feedback>();
 
   feedback->actions.resize(1);
+  auto name      = this->get_parameter("name").as_string();
+
 
   // TODO(speralta): Read in onnx module and run inference
   py::gil_scoped_acquire acquire;
-  py::dict result = inference_(visualize,
+  py::list result_list;
+    if (name == "teleop") {
+result_list = inference_(visualize,
                                py::arg("instruction") = get_instruction(),
                                py::arg("image")       =           py::cast(image_primary),
-                               py::arg("image_wrist") = py::cast(image_wrist));
+                                py::arg("name") =           py::cast(name)
+                          );
+  } else {
+result_list = inference_(visualize,
+                               py::arg("instruction") = get_instruction(),
+                               py::arg("image")       =           py::cast(image_primary)
+                          );
+  }
+  py::dict result = result_list[0].cast<py::dict>();
+  py::print(result.attr("keys")());
+
+  // feedback->actions[0].left_hand.x =
+  // feedback->actions[0].left_hand.y = result["y"].cast<float>();
+  // feedback->actions[0].left_hand.z = result["z"].cast<float>();
+
+  std::vector<float> gripper_pose = result["left_gripper"]["pose"]["xyz"].cast<std::vector<float> >();
+  std::vector<float> gripper_rpy  = result["left_gripper"]["pose"]["rpy"].cast<std::vector<float> >();
+  // feedback->actions[0].left_hand.roll  = result["roll"].cast<float>();
+  // feedback->actions[0].left_hand.pitch = result["pitch"].cast<float>();
+  // feedback->actions[0].left_hand.yaw   = result["yaw"].cast<float>();
+  feedback->actions[0].left_hand.x = gripper_pose[0];
+  feedback->actions[0].left_hand.y = gripper_pose[1];
+  feedback->actions[0].left_hand.z = gripper_pose[2];
+  feedback->actions[0].left_hand.roll  = gripper_rpy[0];
+  feedback->actions[0].left_hand.pitch = gripper_rpy[1];
+  feedback->actions[0].left_hand.yaw   = gripper_rpy[2];
+  feedback->actions[0].left_hand.grasp = result["left_gripper"]["grasp"]["grasp"].cast<float>();
 
 
-  feedback->actions[0].left_hand.x = result["x"].cast<float>();
-  feedback->actions[0].left_hand.y = result["y"].cast<float>();
-  feedback->actions[0].left_hand.z = result["z"].cast<float>();
-
-
-  feedback->actions[0].left_hand.roll  = result["roll"].cast<float>();
-  feedback->actions[0].left_hand.pitch = result["pitch"].cast<float>();
-  feedback->actions[0].left_hand.yaw   = result["yaw"].cast<float>();
-
-  feedback->actions[0].left_hand.grasp = result["grasp"].cast<float>();
+  // feedback->actions[0].left_hand.grasp = result["grasp"].cast<float>();
 
   RCLCPP_WARN(this->get_logger(),
               "feedback: %f, %f, %f, %f, %f, %f, %f",
